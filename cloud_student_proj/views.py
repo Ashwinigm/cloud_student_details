6657ff27f7cb1f537117b7c2f24a0e7eca0f748d
Ashwini Managuli <ashwinimanaguli@Ashwinis-MacBook-Pro.local>
2021-09-20 16:59:48 -0700

    Added frontend

--- cloud_student_proj/views.py
+++ cloud_student_proj/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render,redirect
+from cloud_student_proj.models import Student
+
+def show(request):
+    students = Student.objects.all()
+    return render(request,"profile.html",{'student':students})
--- student_info/urls.py
+++ student_info/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from cloud_student_proj import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('show/',views.show)
 ]
